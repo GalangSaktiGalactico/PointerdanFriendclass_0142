4882700144a87901c80a0c01da3fd8b8c542128e
Galang Sakti G <wadepakmen97@gmail.com>
2024-05-06 10:55:23 +0700

    class siswa

--- ClassPointer/ClassPointer.cpp
+++ ClassPointer/ClassPointer.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+class mahasiswa {
+public:
+	int nim;
+	void shownim() {
+		cout << "No Induk = " << nim << endl;
+	}
+};
+ 
+int main() {
+    mahasiswa mhs{ 1 }; //Object mhs
+    mhs.shownim(); //Member Access Operator
+
+    mahasiswa& refMhs = mhs; //Pointer References refMhs
+    refMhs.nim = 2; //Membber Access Operator
+    mhs.shownim();
+
+    mahasiswa* pMhs = &mhs; //pointer Dereferences pMhs
+    pMhs->nim = 3; //Arrow Operator
+    mhs.shownim();
+    return 0;
 }
 
---
+++ FriendClass/FriendClass.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+class orang;
+{
+private:
+	string nama;
+
+
+public:
+	void setNama(string pNama);
+	friend class siswa;
+
+};
+
+class siswa
+{
+private:
+	int id;
+public:
+	void setid(int pId);
+	void displayA11(orang& a);
+
+};
+
+
+
+
